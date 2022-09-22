e1f50c2f8c8a27a67d3045223ee1ea7c871db19c
panji97 <panjisetyokurniawan@gmail.com>
2022-09-22 18:22:13 +0700

    Design tour part 1

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,37 +1,66 @@
-const Tour = require('./../models/tourModel');
-const catchAsync = require('./../utils/catchAsync');
-// const AppError = require('../utils/AppError');
+const Tour = require('../models/tourModel');
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/AppError');
 
-exports.getDashboard = catchAsync(async (req, res, next) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Panji'
-  });
-});
-
-exports.getOverview = catchAsync(async (req, res) => {
-  //1) get tour data from collection
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1) Get tour data from collection
   const tours = await Tour.find();
 
-  //2)build template
-  //3) render that template using tour data from 1)
+  // 2) Build template
+  // 3) Render that template using tour data from 1)
   res.status(200).render('overview', {
     title: 'All Tours',
     tours
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
-  //1) get the data, for the requested tour (including reviews and guides)
+exports.getTour = catchAsync(async (req, res, next) => {
+  // 1) Get the data, for the requested tour (including reviews and guides)
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
-    path: 'revies',
+    path: 'reviews',
     fields: 'review rating user'
   });
-  //2) build template
 
-  //3) render template using data from 1)
+  if (!tour) {
+    return next(new AppError('There is no tour with that name.', 404));
+  }
+
+  // 2) Build template
+  // 3) Render template using data from 1)
   res.status(200).render('tour', {
-    title: 'The Forest Hiker',
+    title: `${tour.name} Tour`,
     tour
+  });
+});
+
+exports.getLoginForm = (req, res) => {
+  res.status(200).render('login', {
+    title: 'Log into your account'
+  });
+};
+
+exports.getAccount = (req, res) => {
+  res.status(200).render('account', {
+    title: 'Your account'
+  });
+};
+
+exports.updateUserData = catchAsync(async (req, res, next) => {
+  const updatedUser = await User.findByIdAndUpdate(
+    req.user.id,
+    {
+      name: req.body.name,
+      email: req.body.email
+    },
+    {
+      new: true,
+      runValidators: true
+    }
+  );
+
+  res.status(200).render('account', {
+    title: 'Your account',
+    user: updatedUser
   });
 });
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const viewController = require('./../controllers/viewController');
+// const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
-router.get('/', viewController.getDashboard);
-
-router.get('/overview', viewController.getOverview);
-
+router.get('/', viewController.getOverview);
 router.get('/tour/:slug', viewController.getTour);
+// router.get('/login', authController.isLoggedIn, viewController.getLoginForm);
+// router.get('/me', authController.protect, viewController.getAccount);
+
+// router.post(
+//   '/submit-user-data',
+//   authController.protect,
+//   viewController.updateUserData
+// );
 
 module.exports = router;
