cc3af38f7cc8a823b6e2362b32ef77446d09ec43
panji97 <panjisetyokurniawan@gmail.com>
2022-09-26 14:35:20 +0700

    Rendering error page (193)

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/AppError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -43,6 +44,7 @@ app.use('/api', limiter);
 
 // body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
 
 // data sanitization against noSQL query injection
 app.use(mongoSanitize());
@@ -56,6 +58,7 @@ app.use(hpp());
 // test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  // console.log(req.cookies);
   next();
 });
 
